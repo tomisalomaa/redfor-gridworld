2b93860453cbeada1a1848f7d9ded5389cd06003
tomisalomaa <salomaa.tomi@gmail.com>
2021-09-08 11:18:46 +0300

    refactored run() out

--- monolith.py
+++ monolith.py
@@ -5,38 +5,14 @@ from agent import Agent
 from q_agent import QAgent
 from environment import Environment
 
-def run(environment, agent, episodes=1000, maxSteps=1000, learn=False):
-    rewardLog = []
-    iteration = 1
-    for episode in range(episodes):
-        cumulativeReward = 0
-        step = 0
-        terminate = False
-        while step < maxSteps and not terminate:
-            oldState = environment.agentLocation
-            a = agent.decideAction(environment.actionSet)
-            reward = environment.step(a)
-            deltaState = environment.agentLocation
-            if learn:
-                agent.updateQtable(oldState, reward, deltaState, a)
-            cumulativeReward += reward
-            step += 1
-            if environment.checkState() == "terminal":
-                environment.__init__()
-                terminate = True
-        rewardLog.append(cumulativeReward)
-        print("Episode", iteration, ":", cumulativeReward)
-        iteration += 1
-    return rewardLog
+env = Environment()
 
-env = Environment(show=True)
+# agent = Agent(env)
+# rewardsAchieved = agent.run()
+# plt.plot(rewardsAchieved)
+# plt.show()
 
-agent = Agent(env.actionSet)
-rewardsAchieved = run(env, agent, episodes=500)
-plt.plot(rewardsAchieved)
-plt.show()
-
-agent = QAgent(env, env.actionSet)
-rewardsAchieved = run(env, agent, 500, learn=True)
+agent = QAgent(env)
+rewardsAchieved = agent.run()
 plt.plot(rewardsAchieved)
 plt.show()
